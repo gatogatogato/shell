833a37a8c0a6dc1492bd84f423c8a7aafef401ce
gatogatogato <tobias.gaulke@gmail.com>
2020-09-18 21:08:30 +0200

    Fix commands (echo)

--- fix-tmux.sh
+++ fix-tmux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 echo "Fix /etc/tmux.conf"
-sudo cat "# remap prefix from 'C-b' to 'C-a'" > /etc/tmux.conf
-sudo cat "unbind C-b" >> /etc/tmux.conf
-sudo cat "set-option -g prefix C-a" >> /etc/tmux.conf
-sudo cat "bind-key C-a send-prefix" >> /etc/tmux.conf
+sudo echo "# remap prefix from 'C-b' to 'C-a'" > /etc/tmux.conf
+sudo echo "unbind C-b" >> /etc/tmux.conf
+sudo echo "set-option -g prefix C-a" >> /etc/tmux.conf
+sudo echo "bind-key C-a send-prefix" >> /etc/tmux.conf
