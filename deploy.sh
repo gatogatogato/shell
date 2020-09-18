a35e8d00ce4c3777237b816a5a75686585d0c8ea
gatogatogato <tobias.gaulke@gmail.com>
2020-09-18 17:20:33 +0200

    Change push to pull

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ TARGET_DIR=~/shell
 echo ${DIVIDER}
 cd ${TARGET_DIR}
 echo "Update from GitHub..."
-git push .
+git pull origin master
 echo ${DIVIDER}
 cp ${TARGET_DIR}/dot-zshrc.txt ~/.zshrc
 echo ${DIVIDER}
