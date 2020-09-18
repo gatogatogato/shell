feb47500809ef7bd3b673546ce5f28e211eda814
gatogatogato <tobias.gaulke@gmail.com>
2020-09-18 17:30:32 +0200

    More progress

--- deploy.sh
+++ deploy.sh
@@ -8,15 +8,23 @@ TARGET_DIR=~/shell
 #
 echo ${DIVIDER}
 cd ${TARGET_DIR}
+
 echo "Update from GitHub..."
 git pull origin master -q
 echo ${DIVIDER}
+
 echo "Make shell files executable..."
 chmod +x ${TARGET_DIR}/*.sh
 echo ${DIVIDER}
+
 echo "Copy files..."
 cp ${TARGET_DIR}/dot-zshrc.txt ~/.zshrc
-echo ${DIVIDER}
+
+if [[ $SHELL -eq "/bin/zsh" ]]; then
+	echo "We are in ZSH. Activate new ~/.zshrc..."
+	source ~/.zshrc
+fi
+
 echo "Took ${SECONDS} seconds to deploy."
 echo "End of script."
 echo ${DIVIDER}
