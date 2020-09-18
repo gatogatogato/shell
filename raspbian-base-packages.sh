90988d0a022a458d61cc20b61a54866718ffec95
gatogatogato <tobias.gaulke@gmail.com>
2020-09-18 18:22:05 +0200

    even quieter

--- raspbian-base-packages.sh
+++ raspbian-base-packages.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env zsh
-echo "Install base packages via apt..."
 #---------------------------------------------------------------------------------------------------
 PACKAGES="apt-transport-https ca-certificates curl debsums gnupg-agent hdparm hercules libglib2.0 mosh mutt nano net-tools nmap reboot-notifier software-properties-common tmux unzip wget"
 #---------------------------------------------------------------------------------------------------
-sudo apt update
-sudo apt upgrade
-sudo apt full-upgrade
+echo "Update apt database and packages..."
+sudo apt -q update
+sudo apt -q upgrade
+sudo apt -q full-upgrade
+
+echo "Install base packages via apt..."
 for PACKAGE in ${PACKAGES}; do
 	echo "Install ${PACKAGE}..."
 	sudo apt-get --yes -qq install ${PACKAGE}
