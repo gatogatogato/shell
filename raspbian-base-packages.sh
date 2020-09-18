869030920f37a57645a573818e9a5fbffa6f376a
gatogatogato <tobias.gaulke@gmail.com>
2020-09-18 18:13:09 +0200

    Base version

---
+++ raspbian-base-packages.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+echo "Install base packages via apt..."
+#---------------------------------------------------------------------------------------------------
+PACKAGES=(apt-transport-https 
+ca-certificates 
+curl 
+debsums 
+gnupg-agent 
+hdparm
+hercules 
+libglib2.0 
+mosh 
+mutt 
+nano 
+net-tools 
+nmap 
+reboot-notifier
+software-properties-common
+tmux 
+unzip
+wget )
+#---------------------------------------------------------------------------------------------------
+sudo apt update
+sudo apt upgrade
+sudo apt full-upgrade
+sudo apt-get --yes install apt-transport-https ca-certificates gnupg-agent software-properties-common
+sudo apt-get --yes install net-tools libglib2.0 nano mutt hdparm
+sudo apt-get --yes install curl wget mosh tmux nmap hercules unzip
+sudo apt-get --yes install debsums reboot-notifier
+for PACKAGE in ${PACKAGES}; do
+	sudo apt-get --yes install ${PACKAGE}
+done
