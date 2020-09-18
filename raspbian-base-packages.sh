ec5a6d19da5341af36c94eab35c5d974345ed4b0
gatogatogato <tobias.gaulke@gmail.com>
2020-09-18 18:20:41 +0200

    apt-get quiet

--- raspbian-base-packages.sh
+++ raspbian-base-packages.sh
@@ -6,11 +6,7 @@ PACKAGES="apt-transport-https ca-certificates curl debsums gnupg-agent hdparm he
 sudo apt update
 sudo apt upgrade
 sudo apt full-upgrade
-sudo apt-get --yes install apt-transport-https ca-certificates gnupg-agent software-properties-common
-sudo apt-get --yes install net-tools libglib2.0 nano mutt hdparm
-sudo apt-get --yes install curl wget mosh tmux nmap hercules unzip
-sudo apt-get --yes install debsums reboot-notifier
 for PACKAGE in ${PACKAGES}; do
 	echo "Install ${PACKAGE}..."
-	sudo apt-get --yes install ${PACKAGE}
+	sudo apt-get --yes -qq install ${PACKAGE}
 done
