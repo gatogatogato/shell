58ae30b7c34f73e009ec51c164b7316db2d1ab1b
gatogatogato <tobias.gaulke@gmail.com>
2020-09-19 13:23:14 +0200

    Enable service

--- raspbian-05-install-docker.sh
+++ raspbian-05-install-docker.sh
@@ -3,3 +3,6 @@ curl -fsSL https://get.docker.com -o ~/get-docker.sh
 sudo sh ~/get-docker.sh
 sudo usermod -aG docker pi
 rm ~/get-docker.sh
+
+sudo systemctl enable docker
+sudo systemctl start docker
