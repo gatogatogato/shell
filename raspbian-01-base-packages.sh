f5d3279d1ebefa05da0b18769a6b4e7f83584b22
gatogatogato <tobias.gaulke@gmail.com>
2020-09-19 09:21:24 +0200

    Fix base package install

--- raspbian-01-base-packages.sh
+++ raspbian-01-base-packages.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env zsh
 #---------------------------------------------------------------------------------------------------
-PACKAGES="apt-transport-https ca-certificates curl debsums gnupg-agent hdparm hercules libglib2.0 mosh mutt nano net-tools nmap reboot-notifier software-properties-common tmux unzip wget"
+# PACKAGES="apt-transport-https ca-certificates curl debsums gnupg-agent hdparm hercules libglib2.0 mosh mutt nano net-tools nmap reboot-notifier software-properties-common tmux unzip wget"
+PACKAGES_FILE="~/shell/zz-raspbian-base-packages.txt"
 #---------------------------------------------------------------------------------------------------
 echo "Update apt database and packages..."
 sudo apt-get -qq update
 sudo apt-get -qq upgrade
 sudo apt-get -qq full-upgrade
 
-echo "Install base packages via apt..."
-for PACKAGE in ${PACKAGES}; do
+# echo "Install base packages via apt..."
+# for PACKAGE in ${PACKAGES}; do
+# done
+
+while IFS= read -r PACKAGE
+do
 	echo "##### Install ${PACKAGE}..."
 	sudo apt-get --yes -q install ${PACKAGE}
-done
+done < "${PACKAGES_FILE}"
