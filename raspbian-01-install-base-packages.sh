6ebf89c74af16e99d2d3ae530b05259c229ae018
gatogatogato <tobias.gaulke@gmail.com>
2020-09-19 11:54:14 +0200

    Slim down the script

--- raspbian-01-install-base-packages.sh
+++ raspbian-01-install-base-packages.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env zsh
 #---------------------------------------------------------------------------------------------------
-# PACKAGES="apt-transport-https ca-certificates curl debsums gnupg-agent hdparm hercules libglib2.0 mosh mutt nano net-tools nmap reboot-notifier software-properties-common tmux unzip wget"
 PACKAGES_FILE="/home/pi/shell/zz-raspbian-base-packages.txt"
 #---------------------------------------------------------------------------------------------------
 echo "Update apt database and packages..."
 sudo apt-get -qq update
 sudo apt-get -qq upgrade
 sudo apt-get -qq full-upgrade
-
-# echo "Install base packages via apt..."
-# for PACKAGE in ${PACKAGES}; do
-# done
 
 while read -r PACKAGE
 do
