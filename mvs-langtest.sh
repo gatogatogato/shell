be630273413404d52f52811ef4067670d5e7f6b6
gatogatogato <tobias.gaulke@gmail.com>
2020-10-07 17:29:48 +0200

    Initial version

--- mvs-langtest.sh
+++ mvs-langtest.sh
@@ -1,8 +1,33 @@
 #!/bin/zsh
 #-----------------------------------------------------------------
-export installdir="~/tk4-"
+export installdir=~/tk4-
 #-----------------------------------------------------------------
 echo "Build langtest for performance testing..."
 cd ${installdir}
+
 git clone "https://github.com/wfjm/mvs38j-langtest"
 mv mvs38j-langtest mvs38jlang
+
+export PATH=${PATH};${installdir}/mvs38jlang/bin
+
+clear
+echo "--------------------------------"
+echo " Next adapt the JCL defaults    "
+echo "--------------------------------"
+echo ""
+echo "Press enter to continue..."
+read
+cd ${installdir}/mvs38jlang/jcl
+nano job_asm_clg.JESI
+cd ${installdir}/mvs38jlang/jobs
+make
+
+
+clear
+echo "--------------------------------"
+echo " Submit JCL with                "
+echo "   hercjis *asm*.jcl            "
+echo "--------------------------------"
+echo ""
+echo "Press enter to continue..."
+read
