f0eff462bc602f44955b0dc8fdb67da7cb17d5b3
gatogatogato <tobias.gaulke@gmail.com>
2020-10-07 18:13:00 +0200

    Improvements

--- mvs-langtest.sh
+++ mvs-langtest.sh
@@ -9,21 +9,12 @@ cd ${installdir}
 git clone "https://github.com/wfjm/mvs38j-langtest"
 git clone "https://github.com/wfjm/herc-tools"
 
-clear
-echo "--------------------------------------------"
-echo " Next adapt the JCL defaults manually first "
-echo "--------------------------------------------"
-echo ""
-echo "Press enter to continue..."
-read
-cd ${installdir}/mvs38j-langtest/jcl
-
 echo "Change job names..."
-sed --in-place=.old -e 's=${JOB}=HERC01JX=g' ${installdir}/*.JESI
+sed --in-place=.old -e 's=\$\{JOB\}=HERC01JX=g' ${installdir}/jcl/*.JESI
 echo "Build JCL..."
+cd ${installdir}/jobs/
 make
 
-clear
 echo "--------------------------------------------"
 echo " Remember to export PATH to                 "
 echo " export PATH=${PATH}:${installdir}/herc-tools/bin"
