a6b0c27efab56905c32a7e8d83d86d457cf25d32
gatogatogato <tobias.gaulke@gmail.com>
2020-10-07 18:33:45 +0200

    Further fixes

--- mvs-langtest.sh
+++ mvs-langtest.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 #-----------------------------------------------------------------
 export installdir=~/
-export PATH=${PATH}:${installdir}/herc-tools/bin
+export PATH=${PATH}:${installdir}/mvs38j-langtest/herc-tools/bin
 #-----------------------------------------------------------------
 echo "Build langtest for performance testing..."
 
@@ -11,6 +11,8 @@ git clone "https://github.com/wfjm/mvs38j-langtest"
 cd ${installdir}/mvs38j-langtest
 git clone "https://github.com/wfjm/herc-tools"
 git clone "https://github.com/wfjm/mvs38j-sios"
+rm -rf sios
+mv mvs38j-sios sios
 
 echo "Change job names..."
 sed --in-place=.old -e 's=${JOB}=HERC01JX=g' ${installdir}/mvs38j-langtest/jcl/*.JESI
