4533db83d184b949136e4c544b5b01123e34c6ec
gatogatogato <tobias.gaulke@gmail.com>
2020-10-07 18:15:50 +0200

    Bug fixes

--- mvs-langtest.sh
+++ mvs-langtest.sh
@@ -10,10 +10,14 @@ git clone "https://github.com/wfjm/mvs38j-langtest"
 git clone "https://github.com/wfjm/herc-tools"
 
 echo "Change job names..."
-sed --in-place=.old -e 's=\$\{JOB\}=HERC01JX=g' ${installdir}/jcl/*.JESI
+sed --in-place=.old -e 's=${JOB}=HERC01JX=g' ${installdir}/mvs38j-langtest/jcl/*.JESI
+sed --in-place=.old -e 's=WFJM=GATO=g' ${installdir}/mvs38j-langtest/jcl/*.JESI
+sed --in-place=.old -e 's=MSGCLASS:-A=MSGCLASS:-H=g' ${installdir}/mvs38j-langtest/jcl/*.JESI
 echo "Build JCL..."
-cd ${installdir}/jobs/
+cd ${installdir}/mvs38j-langtest/jobs/
 make
+
+
 
 echo "--------------------------------------------"
 echo " Remember to export PATH to                 "
