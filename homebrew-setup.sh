222b1db8cf6dda0c2ac4c60ff81bb8d390ec28d9
gatogatogato <tobias.gaulke@gmail.com>
2021-05-29 19:10:40 +0200

    Cleaner syntax
    We should have them in ""

--- homebrew-setup.sh
+++ homebrew-setup.sh
@@ -16,35 +16,35 @@ read
 
 #---------------------------------------------------------------------------------------------------
 echo "Install basic stuff from brew ..."
-brew install imagemagick
-brew install youtube-dl
-brew install exiftool
-brew install nmap
-brew install ffmpeg
-brew install mosh
-brew install topgrade
-brew install htop
-brew install zsh-completions
-brew install bash-completion
+brew install "imagemagick"
+brew install "youtube-dl"
+brew install "exiftool"
+brew install "nmap"
+brew install "ffmpeg"
+brew install "mosh"
+brew install "topgrade"
+brew install "htop"
+brew install "zsh-completions"
+brew install "bash-completion"
 
 #---------------------------------------------------------------------------------------------------
 echo "Install desktop apps from brew ..."
 brew install tor-browser
 brew install mactracker
-brew install --cask catch
-brew install --cask github
-brew install --cask the-unarchiver
-brew install --cask appcleaner
-brew install --cask signal
-brew install --cask handbrake
-brew install --cask vlc
-brew install --cask transmission
-brew install --cask sublime-text
-brew install --cask 1Password
-brew install --cask Dropbox
-brew install --cask coconutbattery
-brew install --cask arq
-brew install --cask Firefox
+brew install --cask "catch"
+brew install --cask "github"
+brew install --cask "the-unarchiver"
+brew install --cask "appcleaner"
+brew install --cask "signal"
+brew install --cask "handbrake"
+brew install --cask "vlc"
+brew install --cask "transmission"
+brew install --cask "sublime-text"
+brew install --cask "1Password"
+brew install --cask "Dropbox"
+brew install --cask "coconutbattery"
+brew install --cask "arq"
+brew install --cask "Firefox"
 
 #---------------------------------------------------------------------------------------------------
 echo "End of script."
