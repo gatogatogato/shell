419b6f109eb1128b71d3bbc9b05598526233d6c8
gatogatogato <tobias.gaulke@gmail.com>
2021-09-04 15:21:35 +0200

    Update update-docker-compose.sh

--- update-docker-compose.sh
+++ update-docker-compose.sh
@@ -96,7 +96,7 @@ services:
     restart: unless-stopped
 #////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   couchpotato:
-    image: ghcr.io/linuxserver/couchpotato
+    image: ghcr.io/linuxserver/couchpotato:armhf
     container_name: couchpotato
     environment:
       - PUID=1000
