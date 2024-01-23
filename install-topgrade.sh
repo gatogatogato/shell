a1b5308f8532e43b61e9324fd05c8a96ab146eee
gatogatogato <tobias.gaulke@gmail.com>
2024-01-23 09:54:50 +0100

    Update install-topgrade.sh
    Refine topgrade install heelper

--- install-topgrade.sh
+++ install-topgrade.sh
@@ -6,8 +6,6 @@ clear
 XNAME=$(hostname)
 RET=$?
 if [[ ${RET} -eq 0 ]]; then
-	mkdir ~/topgrade
-	cd ~/topgrade
 	if [[ "$XNAME" == *"raspberrypi"* ]]; then
 		echo "Running on raspberrypi"
 		FILE=topgrade-v14.0.0-aarch64-unknown-linux-musl.tar.gz
@@ -18,7 +16,16 @@ if [[ ${RET} -eq 0 ]]; then
 		echo "UNKNOWN"
 	fi
 	wget https://github.com/topgrade-rs/topgrade/releases/download/v14.0.0/${FILE}
-	tar xzvf ${FILE} && rm ${FILE}
+
+	if [ -d "~/topgrade" ]; then
+	  echo "Topgrade directory exists. Renaming..."
+	  TS=$(date +"%Y-%m-%d_%H.%M.%S")
+	  mv ~/topgrade ~/topgrade-${TS}
+	else
+		mkdir ~/topgrade
+		cd ~/topgrade
+		tar xzvf ${FILE} && rm ${FILE}
+	fi
 	echo "Run with ~/topgrade/topgrade"
 else
 	echo "Could not determine OS."
