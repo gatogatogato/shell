454918b217a307a2108d6664389bc8a6738fbfb1
gatogatogato <tobias.gaulke@gmail.com>
2024-01-23 09:38:07 +0100

    Update install-topgrade.sh
    Change topgrade version to 14.0.0

--- install-topgrade.sh
+++ install-topgrade.sh
@@ -3,22 +3,21 @@ SECONDS=0
 echo "---------------------------------------------------"
 echo "Install topgrade on Debian or Raspbian"
 clear
-XNAME=$(grep "^NAME=" /etc/os-release)
-ARCHNAME=$(uname -a)
+XNAME=$(hostname)
 RET=$?
 if [[ ${RET} -eq 0 ]]; then
 	mkdir ~/topgrade
 	cd ~/topgrade
-	if [[ "$ARCHNAME" == *"aarch64"* ]]; then
-		echo "RASPIAN ARM"
-		FILE=topgrade-v13.0.0-aarch64-unknown-linux-gnu.tar.gz
+	if [[ "$XNAME" == *"raspberrypi"* ]]; then
+		echo "Running on raspberrypi"
+		FILE=topgrade-v14.0.0-aarch64-unknown-linux-musl.tar.gz
 	elif [[ "$XNAME" == *"Debian"* ]]; then
 		echo "DEBIAN"
-		FILE=topgrade-v13.0.0-x86_64-unknown-linux-gnu.tar.gz
+		FILE=topgrade-v14.0.0-x86_64-unknown-linux-gnu.tar.gz
 	else
 		echo "UNKNOWN"
 	fi
-	wget https://github.com/topgrade-rs/topgrade/releases/download/v13.0.0/${FILE}
+	wget https://github.com/topgrade-rs/topgrade/releases/download/v14.0.0/${FILE}
 	tar xzvf ${FILE} && rm ${FILE}
 	echo "Run with ~/topgrade/topgrade"
 else
@@ -28,5 +27,3 @@ fi
 echo "---------------------------------------------------"
 echo "Update took ${SECONDS} seconds to finish."
 echo "---------------------------------------------------"
-
-
