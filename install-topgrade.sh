6f0911e9dee7dfc86d75bab15c4e1a41540ad712
gatogatogato <tobias.gaulke@gmail.com>
2024-04-14 18:35:51 +0200

    Changes on old scripts
    Changes on old scripts, remove unneeded scripts

--- install-topgrade.sh
+++ install-topgrade.sh
@@ -4,16 +4,16 @@ echo "---------------------------------------------------"
 echo "Install topgrade on Debian or Raspbian"
 clear
 INSTALLDIR=~/topgrade
-BASEURL=https://github.com/topgrade-rs/topgrade/releases/download/v14.0.0
+BASEURL=https://github.com/topgrade-rs/topgrade/releases/download/v14.0.1
 XNAME=$(hostname)
 RET=$?
 if [[ ${RET} -eq 0 ]]; then
 	if [[ "$XNAME" == *"raspberrypi"* ]]; then
 		echo "Running on raspberrypi"
-		FILE=topgrade-v14.0.0-aarch64-unknown-linux-musl.tar.gz
+		FILE=topgrade-v14.0.1-aarch64-unknown-linux-musl.tar.gz
 	elif [[ "$XNAME" == *"Debian"* ]]; then
 		echo "DEBIAN"
-		FILE=topgrade-v14.0.0-x86_64-unknown-linux-gnu.tar.gz
+		FILE=topgrade-v14.0.1-x86_64-unknown-linux-gnu.tar.gz
 	else
 		echo "UNKNOWN"
 	fi
