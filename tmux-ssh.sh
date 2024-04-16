191d73f7c95b21950d7bd7f4b28f0da4371bb0ff
gatogatogato <tobias.gaulke@gmail.com>
2024-04-16 21:35:28 +0200

    New script for MacOs
    ...to open SSH sessions to all VMs and Containers at once

---
+++ tmux-ssh.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+serverNames="websrv flickr test"
+counter=0
+serverCount=$(echo "${serverNames}" | wc -w | xargs)
+
+echo "serverCount is ${serverCount}"
+
+tmux -2 attach-session -t gato
+if [[ $? -eq 1 ]]; then
+        # tmux session is not active - we'll create one 
+        for server in ${serverNames}
+        do
+                counter=$((counter + 1))
+                echo "Counter is ${counter}, server is ${server}"
+
+                if [[ ${counter} -eq 1 ]]; then      
+                        tmux new-session -s gato -n "${server}" -d "mosh gato@debian-${server}.local"
+                        tmux rename-window -t gato:1 "${server}"
+                else
+                        tmux new-window -t gato:${counter} -n "${server}" "mosh gato@debian-${server}.local"
+                        tmux rename-window -t gato:${counter} "${server}"
+                fi
+        done
+fi
+tmux select-window -t gato:0
+tmux -2 attach-session -t gato
