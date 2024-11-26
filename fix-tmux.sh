95d71d3b386315f097329cc16249661d5fda1e09
gatogatogato <tobias.gaulke@gmail.com>
2024-11-26 20:50:28 +0100

    Extend script for tmux

--- fix-tmux.sh
+++ fix-tmux.sh
@@ -3,9 +3,14 @@ echo "Fix /etc/tmux.conf"
 echo "New location is in /opt/homebrew/etc/tmux.conf"
 
 outfile=/opt/homebrew/etc/tmux.conf
+private_outfile=~/.tmux.conf
 
 sudo echo "# remap prefix from 'C-b' to 'C-a'" > ${outfile}
 sudo echo "unbind C-b" >> ${outfile}
 sudo echo "set-option -g prefix C-a" >> ${outfile}
 sudo echo "bind-key C-a send-prefix" >> ${outfile}
 
+echo "# Press Ctrl-a Ctrl-x to send a command to all panes"  >  ${private_outfile}
+echo "# Press Ctrl-a Mac-x to disable this mode"             >> ${private_outfile}
+echo "bind -n C-x setw synchronize-panes on"                 >> ${private_outfile}
+echo "bind -n M-x setw synchronize-panes off"                >> ${private_outfile}
