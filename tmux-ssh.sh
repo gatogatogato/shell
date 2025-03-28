19abaad3be3ff7f71abc3ab07e2fd17ce4c0ebc0
gatogatogato <tobias.gaulke@gmail.com>
2025-03-28 13:10:08 +0100

    LOL

--- tmux-ssh.sh
+++ tmux-ssh.sh
@@ -16,7 +16,7 @@ readonly SERVERS=(
     debian-ansible
     debian-changedetection
     debian-uptimekuma
-    debian-pihole
+    debian-technitium
     debian-test1
     debian-test2
 )
@@ -30,7 +30,7 @@ fi
 # Function to check if server is reachable
 check_server() {
     local server="${1}"
-    ping -c 1 -W "${PING_TIMEOUT}" "${server}.local" &> /dev/null
+    ping -c 5 -W "${PING_TIMEOUT}" "${server}.lan" &> /dev/null
 }
 
 # Try to attach to existing session first
@@ -43,9 +43,9 @@ echo "Creating new tmux session with connections to servers..."
 
 # Initialize first window
 if check_server "${SERVERS[0]}"; then
-    tmux new-session -s "${SESSION_NAME}" -n "${SERVERS[0]}" -d "mosh gato@${SERVERS[0]}.local || bash"
+    tmux new-session -s "${SESSION_NAME}" -n "${SERVERS[0]}" -d "mosh gato@${SERVERS[0]}.lan"
 else
-    echo "Warning: ${SERVERS[0]}.local is not reachable"
+    echo "Warning: ${SERVERS[0]}.lan is not reachable"
     tmux new-session -s "${SESSION_NAME}" -n "${SERVERS[0]}" -d "echo 'Server ${SERVERS[0]} is not reachable'; sleep 10; tmux kill-window"
 fi
 
@@ -54,9 +54,9 @@ for server in "${SERVERS[@]:1}"; do
     window_number=$(($(tmux list-windows -t "${SESSION_NAME}" | wc -l) + 1))
     
     if check_server "${server}"; then
-        tmux new-window -t "${SESSION_NAME}:${window_number}" -n "${server}" "mosh gato@${server}.local || bash"
+        tmux new-window -t "${SESSION_NAME}:${window_number}" -n "${server}" "mosh gato@${server}.lan"
     else
-        echo "Warning: ${server}.local is not reachable"
+        echo "Warning: ${server}.lan is not reachable"
         tmux new-window -t "${SESSION_NAME}:${window_number}" -n "${server}" "echo 'Server ${server} is not reachable'; sleep 10"
     fi
 done
